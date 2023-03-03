f2914411063abc6fb5d29086d9d848cb0ec3df1a
C(h)icchiD'Uva <75309352+alessandrocicchi@users.noreply.github.com>
2023-03-03 09:21:30 +0100

    3 Marzo 2023

---
+++ movieRewiew/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {RouterView } from 'vue-router'
+import Navbar from './views/Navbar.vue'
+import Container from './views/Container.vue'
+</script>
+
+<template>
+  <div>
+    <div>
+      <Navbar />
+    </div>
+    <tbody>
+    <div>
+      <Container />
+    </div>
+  </tbody>
+  </div>    
+  <RouterView />
+</template>
+
+<style>
+
+</style>
